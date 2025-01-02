c9987ff99525724ead4c25d08afe42955211dbff
SamuP14 <samuelperfal20@gmail.com>
2025-01-02 15:11:03 +0000

    cambios menores

--- users/forms.py
+++ users/forms.py
@@ -13,7 +13,7 @@ class EditProfileForm(forms.ModelForm):
         widgets = {
             'bio': forms.Textarea(
                 attrs={
-                    'style': 'width: 100%; height: 300px;',
+                    'style': 'width: 100%; height: 350px;',
                     'class': 'form-control',
                     'placeholder': 'Enter your bio here...',
                 }
