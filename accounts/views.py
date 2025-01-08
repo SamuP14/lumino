be6e87c9167d8f63291ecb3fbf3ab3eebd75d4e7
SamuP14 <samuelperfal20@gmail.com>
2025-01-08 20:28:55 +0000

    cambios menores

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.models import Profile
-
 from .forms import LoginForm, SignupForm
 
 
@@ -39,7 +37,6 @@ def user_signup(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
-            Profile.objects.create(user=user)
             login(request, user)
             return redirect('index')
     else:
---
+++ users/signals.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import Profile
+
+
+@receiver(post_save, sender=User)
+def create_user_profile_with_new_user(sender, instance, created, raw, **kwargs):
+    if created:
+        if not raw:
+            profile = Profile(user=instance)
+            instance.profile = profile
+            instance.save()
